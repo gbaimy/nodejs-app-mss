8652d89906ebaf1f9827ce184a92f93d4b99f9e6
gbaimy <pooly_promise@yahoo.com>
2024-09-25 13:32:39 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -9,7 +9,7 @@ sonarqubeScanner({
 	    'sonar.login': 'squ_e2237ad77934d4fe7f6be147c53819acb1079580',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'8.3.0.34182',
+            'sonar.projectVersion':'1.0.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
