e4a64c696180dc139a5174eeb72cc2a0d87259c1
gbaimy <pooly_promise@yahoo.com>
2024-09-26 11:22:04 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://75.101.226.200:9000/',
+  serverUrl: 'http://54.196.216.228:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
