a1182fb359c6e718696e400e50444224e4d3d662
Gbemisola Akinyemi <pooly_promise@yahoo.com>
2025-01-10 12:48:53 +0100

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,19 +1,19 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://localhost:9981/',
+  serverUrl: 'http://localhost:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_e2237ad77934d4fe7f6be147c53819acb1079580',
-	    'sonar.login': 'admin',
+	    'sonar.token': 'squ_e2237ad77934d4fe7f6be147c53819acb1079580',
+	    'sonar.token': 'admin',
 	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
+	  //'sonar.test': 'specs',
           //'sonar.inclusions' : 'src/**'
        },
 }, () => {});
